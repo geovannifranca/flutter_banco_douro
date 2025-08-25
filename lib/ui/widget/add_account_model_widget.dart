1c37cbbc5423d898024e66233dcd92f57b18318b
geovannifranca <geovannidefranca98@gmail.com>
2025-08-25 19:46:02 -0300

    dropdown

--- lib/ui/widget/add_account_model_widget.dart
+++ lib/ui/widget/add_account_model_widget.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_banco_douro/ui/styles/app_colors.dart';
 
-class AddAccountModelWidget extends StatelessWidget {
+class AddAccountModelWidget extends StatefulWidget {
   const AddAccountModelWidget({super.key});
+
+  @override
+  State<AddAccountModelWidget> createState() => _AddAccountModelWidgetState();
+}
+
+class _AddAccountModelWidgetState extends State<AddAccountModelWidget> {
+  String _accountType = "ANBROSIA";
+
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Container(
-      padding:  EdgeInsets.only(
+      padding: EdgeInsets.only(
         left: 32,
         top: 32,
         right: 32,
@@ -21,7 +29,10 @@ class AddAccountModelWidget extends StatelessWidget {
           children: [
             const SizedBox(height: 16),
             Center(
-              child: Image.asset("assets/images/icon_add_account.png", width: 64),
+              child: Image.asset(
+                "assets/images/icon_add_account.png",
+                width: 64,
+              ),
             ),
             const SizedBox(height: 32),
             const Text(
@@ -34,12 +45,34 @@ class AddAccountModelWidget extends StatelessWidget {
               style: TextStyle(fontWeight: FontWeight.w400, fontSize: 14),
             ),
 
-            TextFormField(),
-            TextFormField(),
+            TextFormField(
+              decoration: InputDecoration(label: const Text("Nome")),
+            ),
+            TextFormField(
+              decoration: InputDecoration(label: const Text("Sobre Nome")),
+            ),
+            const SizedBox(height: 16),
+            Text("Tipo da Conta"),
+            DropdownButton<String>(
+              value: _accountType,
+              isExpanded: true,
+              items: const [
+                DropdownMenuItem(value: "ANBROSIA", child: Text("ANBROSIA")),
+                DropdownMenuItem(value: "CANGICA",child: Text("CANGICA")),
+                DropdownMenuItem(value: "PUDIM",child: Text("PUDIM")),
+                DropdownMenuItem(value: "BRIGADEIRO",child: Text("item 3")),
+              ],
+              onChanged: (value) {
+                if (value != null) {
+                  setState(() {
+                    _accountType = value;
+                  });
+                }
+              },
+            ),
             const SizedBox(height: 32),
             Row(
               children: [
-
                 Expanded(
                   child: ElevatedButton(
                     style: ButtonStyle(
